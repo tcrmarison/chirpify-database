a7e996ae93799b1c9706c235f26e2799e2a0878c
TCR - Marison <112858024+tcrmarison@users.noreply.github.com>
2023-02-27 12:50:37 +0100

    Add files via upload

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+
+$username = "root";
+$password = "";
+
+try {
+    $conn =  new PDO(dsn:"mysql:host=localhost;dbname=chirpifydb", $username, $password);
+
+    $get_tweets = $conn->prepare(query:"SELECT * FROM tweets");
+    $get_tweets->execute();
+    $tweets = $get_tweets->fetchAll();
+
+    foreach ($tweets as $tweet) {
+        echo $tweet["body"];
+    }
+    
+} catch(PDOException $error) {
+    echo $error->getMessage();
+}
+
+?>
